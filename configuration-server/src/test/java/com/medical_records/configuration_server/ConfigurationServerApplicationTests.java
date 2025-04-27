a437c7e7c36a65fce4026b18a6ecf1b60efc8c27
vladodev03 <vladsto101@gmail.com>
2025-04-27 20:53:32 +0300

    Added configuration server.

---
+++ configuration-server/src/test/java/com/medical_records/configuration_server/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical_records.configuration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
